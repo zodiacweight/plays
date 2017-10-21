47c4ece0d074baa3f1fbb771346dbcf0b42cdc5a
srgg6701 <srgg67@gmail.com>
2017-10-21 14:32:33 +0300

    Recreation act

---
+++ js/common.js
@@ -0,0 +1,62 @@
+
+$(function(){
+    const classFade = 'fade';
+    const classFadeOut = 'fade-out';
+    const persons = [];
+    const contentsBlockId = 'chapter_filters';
+    const classAlien = 'alien';
+    const parentTagName = 'main';
+    const inputBox = 'input[type="checkbox"]';
+
+    let personName; 
+
+    $(parentTagName).on('mouseenter mouseleave', '#asides aside', event => {
+        (event.type === 'mouseenter') ?
+            $(event.currentTarget).addClass(classFade).removeClass(classFadeOut)
+            : $(event.currentTarget).addClass(classFadeOut);
+    }).on('click', `#${contentsBlockId} ${inputBox}`, event => {
+        // if there are no checked boxes, make all unmarked
+        // main chapter_filters input[type="checkbox"]:checked
+        if (!$(`${parentTagName} #${contentsBlockId} ${inputBox}:checked`).length){
+            // .alien
+            $(`.${classAlien}`).removeClass(classAlien);
+            return;
+        }
+        personName = event.currentTarget.name; //console.log('checked, event ', $(`${parentTagName} #${contentsBlockId} ${inputBox}:checked`).length, event);
+        // add or remove block
+        (event.currentTarget.checked) ? persons.push(personName) : persons.splice(persons.indexOf(personName));
+        $('[data-person]').each((index, personTag) => {
+            //
+            let action = (persons.indexOf(personTag.dataset.person) === -1) ? 'addClass' : 'removeClass';
+            $(personTag)[action](classAlien);
+            $(personTag).next()[action](classAlien);
+        });
+    });
+    const titleTagName = "span";
+    $("#chapters p").on('mouseenter mouseleave', function(event){
+        const $p = $(this);
+        //
+        if (event.type === "mouseenter"){ //console.log('check');
+            const $pA = $p.find("a");
+            if (parseInt($p.css("width")) < parseInt($pA.css("width"))){
+                const offsetLeft = $p.offset().left,
+                    offsetTop = $p.position().top,
+                    title = $(`<${titleTagName}></${titleTagName}>`)
+                        .text($pA.text())
+                        .addClass("title")
+                        .css({
+                            position: "fixed",
+                            left: `${offsetLeft}px`,
+                            top: `${offsetTop}px`
+                        });
+                $p.append(title);
+            }
+        } else {
+            $p.find(titleTagName).remove();
+        }
+    });
+    $('p').on('click', titleTagName, function(){
+        // either trigger nor triggerHandler don't work :( https://stackoverflow.com/questions/20928915/jquery-triggerclick-not-working
+        $(this).parent('p').find('a')[0].click();
+     });
+});
